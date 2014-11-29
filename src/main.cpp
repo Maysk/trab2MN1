8ffe3eef8664ca275627e5b32bbf507e4ed11df9
Caio Viktor Avila <caioviktor@hotmail.com>
2014-11-29 16:29:06 -0300

    Resultado de Gauss na tabela

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,30 @@
 
 int main(int argc, char *argv[])
 {
-/*
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
 
     return a.exec();
-*/
 
-    Matrix *iTM = new Matrix(3,3);
-    Matrix *cM = new Matrix (3,1);
 
-    iTM->setValue(0,0,10);    iTM->setValue(0,1,1);    iTM->setValue(0,2,1);
-    iTM->setValue(1,0,1);     iTM->setValue(1,1,10);   iTM->setValue(1,2,1);
-    iTM->setValue(2,0,1);     iTM->setValue(2,1,1);    iTM->setValue(2,2,10);
+//    Matrix *iTM = new Matrix(3,3);
+//    Matrix *cM = new Matrix (3,1);
 
-    cM->setValue(0,0,12);
-    cM->setValue(1,0,12);
-    cM->setValue(2,0,12);
+//    iTM->setValue(0,0,10);    iTM->setValue(0,1,1);    iTM->setValue(0,2,1);
+//    iTM->setValue(1,0,1);     iTM->setValue(1,1,10);   iTM->setValue(1,2,1);
+//    iTM->setValue(2,0,1);     iTM->setValue(2,1,1);    iTM->setValue(2,2,10);
 
-    cM->printMatrix();
-    iTM->printMatrix();
+//    cM->setValue(0,0,12);
+//    cM->setValue(1,0,12);
+//    cM->setValue(2,0,12);
 
-    Gauss* g = new Gauss(iTM, cM);
+//    cM->printMatrix();
+//    iTM->printMatrix();
 
-    g->resolveSytem();
-    g->getUnknownsMatrix()->printMatrix();
+//    Gauss* g = new Gauss(iTM, cM);
+
+//    g->resolveSytem();
+//    g->getUnknownsMatrix()->printMatrix();
  }
