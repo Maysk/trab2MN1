69ad64cc1e01a62e393abed39913448049f6f8f3
cristmelo <cristianomelo_88@hotmail.com>
2014-11-29 18:22:10 -0700

    Gauss_Jordan COM pivotação parcial
    OK

--- lib/gaussjordan.h
+++ lib/gaussjordan.h
@@ -1,8 +1,12 @@
 class GaussJordan : public GaussTemplate{
-
+    Matrix* coefficientMatrixTemp;
+    Matrix* independentTermsMatrixTemp;
 public:
     GaussJordan(Matrix* independentTermsMatrix, Matrix* coefficientMatrix);
 
+    void beforeSolve();
     void resolveSytem();
+    void afterSolve();
 
+    void pivoting(Matrix* A, Matrix* b, int numberOfLines, int k);
 };
--- src/gaussJordan.cpp
+++ src/gaussJordan.cpp
@@ -3,12 +3,19 @@
 GaussJordan::GaussJordan(Matrix* independentTermsMatrix, Matrix* coefficientMatrix)
     :GaussTemplate(independentTermsMatrix, coefficientMatrix){}
 
+void GaussJordan::beforeSolve(){
+    resetList();
+    this->independentTermsMatrixTemp = getIndependentTerms()->getCopy();
+    this->coefficientMatrixTemp = getCoefficienMatrix()->getCopy();
+}
+
 
 void GaussJordan::resolveSytem(){
     int numberOfLines;
     double multiplier;
     double newValue_aij;
     double newValue_bi;
+
     int i, j, k;
 
     beforeSolve();
@@ -18,9 +25,16 @@ void GaussJordan::resolveSytem(){
 
     numberOfLines = independentTerms->getHeight();
 
-    saveOnList("Sistema Inicial: ");
-
     for( k = 0; k < numberOfLines; k++ ){
+        saveOnList("Operação realizada nesse ponto");
+
+        independentTerms->printMatrix();
+        coefficients->printMatrix();
+
+        if( independentTerms->getValue(k,k) == 0 ){
+            pivoting( independentTerms, coefficients, numberOfLines, k );
+
+        }
 
 
         if( k < numberOfLines-1 ){
@@ -35,32 +49,59 @@ void GaussJordan::resolveSytem(){
         coefficients->setValue( k, 0, newValue_bi );
         independentTerms->setValue( k, k, 1 );
 
-//        cout << "-----------------ITERATION - K = " << k << endl;
-
-//        independentTerms->printMatrix();
-
-//        coefficients->printMatrix();
-
-
         for( i = 0; i < numberOfLines; i++ ){
             if( i != k ){
                 if( k < numberOfLines-1 ){
                     for( j = k + 1; j < numberOfLines; j++ ){
 
-                        newValue_aij = independentTerms->getValue( i, j ) - independentTerms->getValue( i, k ) * independentTerms->getValue( k, j );
+                        newValue_aij = independentTerms->getValue( i, j ) -
+                                independentTerms->getValue( i, k ) * independentTerms->getValue( k, j );
                         independentTerms->setValue( i, j, newValue_aij );
                     }
                 }
 
-                newValue_bi = coefficients->getValue( i, 0 ) - independentTerms->getValue( i, k ) * coefficients->getValue( k, 0 );
+                newValue_bi = coefficients->getValue( i, 0 ) -
+                        independentTerms->getValue( i, k ) * coefficients->getValue( k, 0 );
                 coefficients->setValue( i, 0, newValue_bi );
                 independentTerms->setValue( i, k, 0 );
             }
         }
 
+        afterSolve();
+
     }
 
-    retroSubstitutions();
-    afterSolve();
 
 }
+
+
+void GaussJordan::afterSolve(){
+    //delete getIndependentTerms();
+    //delete getCoefficienMatrix();
+    setIndependentTerms(this->independentTermsMatrixTemp);
+    setCoefficienMatrix(this->coefficientMatrixTemp);
+}
+
+
+    void GaussJordan::pivoting(Matrix* A, Matrix* b, int numberOfLines, int k){
+           double max = A->getValue(k,k);
+           int index = k;
+           for( int i = 0; i < numberOfLines; i++){
+               if( i != k ){
+                   if ( fabs( A->getValue( i, k ) ) > fabs( max ) ){
+                        max = A->getValue( i, k );
+                        index = i;
+                   }
+
+               }
+           }
+           double temp1, temp2;
+           for( int j = 0; j < numberOfLines; j++ ){
+                temp1 = A->getValue( k, j );
+                A->setValue( k, j, A->getValue( index, j) );
+                A->setValue( index, j, temp1 );
+           }
+           temp2 = b->getValue( k, 0 );
+           b->setValue( k, 0, b->getValue( index, 0) );
+           b->setValue( index, 0, temp2);
+    }
